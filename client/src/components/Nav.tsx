b6abae2ff6248b23e3c764a7ad9fd51631804119
William Martinez <wrmartinez709@gmail.com>
2024-07-20 20:21:00 -0400

    add responsive state to nav-brand

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -5,8 +5,9 @@ const Nav = () => {
   return (
     <nav className="nav flex justify-between items-center p-3 pl-4 border-b-[1px]">
       <div className="nav-brand">
-        <Link to="/">
+        <Link className="flex gap-0.2 items-center" to="/">
           <img className="w-[38px]" src={gmLogo} alt="gathering minds logo" />
+          <h2 className="nav-brand-name hidden md:block text-[#ff5131] text-2xl font-semibold">athering Minds</h2>
         </Link>
       </div>
       <ul className="nav-links flex gap-5 text-sm md:text-base">
