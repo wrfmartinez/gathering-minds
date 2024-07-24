a688e07d9f5a18c785cdc127c7221a4910ea2742
William Martinez <wrmartinez709@gmail.com>
2024-07-23 23:31:33 -0400

    setup basic server

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
