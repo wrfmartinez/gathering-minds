93f39bec0a96d389f342d6d24e0bd4df52c12a5b
William Martinez <wrmartinez709@gmail.com>
2024-07-28 19:39:26 -0400

    add database connection and processenv interface

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      MONGODB_URI: string;
+    }
+  }
+}
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected: ${mongoose.connection.name}`);
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
