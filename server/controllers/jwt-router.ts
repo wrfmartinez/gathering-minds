f7551b3ab1e227546fb48e64881009d373d6f36e
William Martinez <wrmartinez709@gmail.com>
2024-07-29 00:19:52 -0400

    add jwt router test routes for authentication

--- server/controllers/jwt-router.ts
+++ server/controllers/jwt-router.ts
@@ -2,4 +2,27 @@ import express from "express";
 import jwt from "jsonwebtoken";
 const router = express.Router();
 
+router.get("/sign-token", (req, res) => {
+  // mock user object
+  const user = {
+    _id: 1,
+    username: "test",
+    password: "test",
+  };
+  // create a token using the sign method
+  const token = jwt.sign({ user }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+router.post("verify-token", (req, res) => {
+  const token: string = req.headers.authorization?.split("")[1] ? req.headers.authorization.split("")[1] : "";
+  // add verify method
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ decoded });
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token." });
+  }
+});
+
 export default router;
--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MONGODB_URI: string;
+      JWT_SECRET: string;
     }
   }
 }
