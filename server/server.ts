69e128dc45ffe82b216ea950dc02fbe946ac6ec7
William Martinez <wrmartinez709@gmail.com>
2024-07-29 00:23:19 -0400

    add cors and jwt route to server.ts

--- server/server.ts
+++ server/server.ts
@@ -13,13 +13,18 @@ declare global {
   }
 }
 
-const app = express();
 const PORT = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected: ${mongoose.connection.name}`);
 });
+
+// ROUTES
+app.use("/jwt-router", JWTRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
